8c8214aec0d37d8a2d7101d2f29169c776a06c40
LeRoy Gardner <leroy.gardner@gmail.com>
2022-10-29 09:26:03 -0400

    Week1

---
+++ about_python.py
@@ -0,0 +1,4 @@
+answers_to_questions = 'The creator of Python is Guido Van Rossum, \n he created Python in December, 1989. \n He is commonly referred to as "BDFL" which stands for "Benevelont Dictator for Life".'
+
+
+print(answers_to_questions)
